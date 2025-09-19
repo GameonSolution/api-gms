12df8dbb42bc5bddedd72fb15dac147b30690b74
Kailash1233 <kailash61203@gmail.com>
2025-09-19 11:45:25 +0530

    db password reset done

--- src/config/typeorm.ts
+++ src/config/typeorm.ts
@@ -1,6 +1,5 @@
-
 import { registerAs } from "@nestjs/config";
-import { config as dotenvConfig } from 'dotenv';
+import { config as dotenvConfig } from "dotenv";
 import { Contact, User } from "../entity";
 import { NewsFeed } from "../entity/news.entity";
 import { Testimonial } from "../entity/testimonials.entity";
@@ -8,28 +7,23 @@ import { DataSource, DataSourceOptions } from "typeorm";
 import { Carousel } from "../entity/carouselImage";
 import { Portfolio } from "../entity/portFolio.entity";
 
-dotenvConfig({ path: '.env' });
+dotenvConfig({ path: ".env" });
 
 const config = {
-  type: 'postgres', // Change this according to your DB (e.g., 'mysql')
-  host: 'aws-0-ap-southeast-1.pooler.supabase.com',
+  type: "postgres", // Change this according to your DB (e.g., 'mysql')
+  host: "aws-0-ap-southeast-1.pooler.supabase.com",
   port: 5432, // Change according to your DB
-  username: 'postgres.bbbzdlyncfcnmzxxaubm',
-  password: 'bcfd4UKJWgFXbBDs',
-  database: 'postgres',
-  entities:  [User,NewsFeed,Contact,Testimonial,Carousel,Portfolio],
-  migrations: ["dist/migrations/*{.ts,.js}"],// Migration path
+  username: "postgres.bbbzdlyncfcnmzxxaubm",
+  password: "hLEqcApVU3b6Z4Xa",
+  database: "postgres",
+  entities: [User, NewsFeed, Contact, Testimonial, Carousel, Portfolio],
+  migrations: ["dist/migrations/*{.ts,.js}"], // Migration path
   synchronize: true, // Set to 'false' in production
   logging: true,
   ssl: { rejectUnauthorized: false }, // Add this line for SSL
+};
 
-
-}
-
-export default registerAs('typeorm', () => config)
+export default registerAs("typeorm", () => config);
 export const connectionSource = new DataSource(config as DataSourceOptions);
-
-
-
 
 //DATABASE_URL=postgresql://postgres.gbkspnwfbhxiwhzzwefg:[YOUR-PASSWORD]@aws-0-ap-south-1.pooler.supabase.com:6543/postgres
